666057a3fd9848c824f6d2d9ccce89707bf27235
dirk <dirk@kooiot.com>
2016-07-23 17:30:03 +0800

    premake4

--- premake4.lua
+++ premake4.lua
@@ -5,10 +5,13 @@ solution "rdc"
 		kind "StaticLib"
 		platforms {"Native"}
 		language "C++"
-		flags {"C++11"}
 		location "build"
-		targetdir "bin/%{cfg.buildcfg}"
+		targetdir "bin"
 		includedirs {"./libs/serial/include"}
+		-- Using cross compile
+		premake.gcc.cc = "arm-linux-gnueabihf-gcc"
+		premake.gcc.cxx = "arm-linux-gnueabihf-g++ -std=c++11"
+		premake.gcc.ar = "arm-linux-gnueabihf-ar"
 
 		files {"./libs/serial/include/**.h", "./libs/serial/src/**.cc" }
 		links { "rt", "pthread"}
@@ -25,16 +28,20 @@ solution "rdc"
 		kind "ConsoleApp"
 		platforms {"Native"}
 		language "C++"
-		flags {"C++11"}
 		defines {"RDC_LINUX_SYS"}
 		location "build"
-		targetdir "bin/%{cfg.buildcfg}"
+		targetdir "bin"
 
 		files {"./Mapper/**.h", "./Mapper/**.cpp" }
 		links { "pthread", "rt", "zmq", "enet", "uv", "serial"}
 		includedirs { ".", "libs/api", "libs/serial/include", "libs/enet/include", "libs/zeromq/include", "libs/libuv-v1.9.1/include" }
 		libdirs {"libs/.libs"}
 
+		-- Using cross compile
+		premake.gcc.cc = "arm-linux-gnueabihf-gcc"
+		premake.gcc.cxx = "arm-linux-gnueabihf-g++ -std=c++11"
+		premake.gcc.ar = "arm-linux-gnueabihf-ar"
+		
 		configuration "Debug"
 			defines { "DEBUG" }
 			flags { "Symbols" }
