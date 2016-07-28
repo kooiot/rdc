4f2acc264992f4118c6c9cdfed7f461c9256cb29
dirk <dirk@kooiot.com>
2016-07-28 18:58:40 +0800

    generate all in server premake

--- premake5_server.lua
+++ premake5_server.lua
@@ -20,6 +20,25 @@ project "common"
 		defines { "NDEBUG" }
 		optimize "On"
 
+project "serial"
+	kind "StaticLib"
+	language "C++"
+	flags {"C++11"}
+	location "build"
+	targetdir "bin/%{cfg.buildcfg}"
+	includedirs {"libs/serial/include"}
+
+	files {"libs/serial/include/**.h", "libs/serial/src/**.cc" }
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
+
 project "api"
 	kind "StaticLib"
 	language "C++"
@@ -92,6 +111,48 @@ project "starter"
 	files {"ServiceStarter/**.h", "ServiceStarter/**.cpp" }
 	links { "pthread", "rt", "zmq", "api", "common", "boost_system", "boost_filesystem"}
 	includedirs { "libs/api", "libs/zeromq/include", "libs/common/include"}
+	libdirs {"libs/.libs"}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
+project "mapper"
+	kind "ConsoleApp"
+	language "C++"
+	flags {"C++11"}
+	defines {"RDC_LINUX_SYS"}
+	location "build"
+	targetdir "bin/%{cfg.buildcfg}"
+
+	files {"./Mapper/**.h", "./Mapper/**.cpp" }
+	links { "dl", "pthread", "rt", "zmq", "enet", "uv", "serial", "api"}
+	includedirs { "libs/api", "libs/common/include", "libs/serial/include", "libs/enet/include", "libs/zeromq/include", "libs/libuv-v1.9.1/include" }
+	libdirs {"libs/.libs"}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
+project "example"
+	kind "SharedLib"
+	language "C++"
+	flags {"C++11"}
+	defines {"RDC_LINUX_SYS"}
+	location "build/plugins"
+	targetdir "bin/%{cfg.buildcfg}/plugins"
+
+	files {"./Plugins/Example/**.h", "./Plugins/Example/**.cpp" }
+	links { "pthread", "rt"}
+	includedirs { "libs/api" }
 	libdirs {"libs/.libs"}
 
 	filter "configurations:Debug"
