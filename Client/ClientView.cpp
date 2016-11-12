786f5d8127aefef176bd3728737f9d218412454a
dirk <dirk@kooiot.com>
2016-11-12 19:13:00 +0800

    for MDI client gui

---
+++ Client/ClientView.cpp
@@ -0,0 +1,102 @@
+// 这段 MFC 示例源代码演示如何使用 MFC Microsoft Office Fluent 用户界面 
+// (“Fluent UI”)。该示例仅供参考，
+// 用以补充《Microsoft 基础类参考》和 
+// MFC C++ 库软件随附的相关电子文档。  
+// 复制、使用或分发 Fluent UI 的许可条款是单独提供的。  
+// 若要了解有关 Fluent UI 许可计划的详细信息，请访问 
+// http://go.microsoft.com/fwlink/?LinkId=238214。
+//
+// 版权所有(C) Microsoft Corporation
+// 保留所有权利。
+
+// ClientView.cpp : CClientView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Client.h"
+#endif
+
+#include "ClientDoc.h"
+#include "ClientView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CClientView
+
+IMPLEMENT_DYNCREATE(CClientView, CTreeView)
+
+BEGIN_MESSAGE_MAP(CClientView, CTreeView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CClientView 构造/析构
+
+CClientView::CClientView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CClientView::~CClientView()
+{
+}
+
+BOOL CClientView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CTreeView::PreCreateWindow(cs);
+}
+
+void CClientView::OnInitialUpdate()
+{
+	CTreeView::OnInitialUpdate();
+
+	CTreeCtrl& ctrl = this->GetTreeCtrl();
+	//ctrl.InsertItem("AAAA");
+}
+
+void CClientView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CClientView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CClientView 诊断
+
+#ifdef _DEBUG
+void CClientView::AssertValid() const
+{
+	CTreeView::AssertValid();
+}
+
+void CClientView::Dump(CDumpContext& dc) const
+{
+	CTreeView::Dump(dc);
+}
+
+CClientDoc* CClientView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CClientDoc)));
+	return (CClientDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CClientView 消息处理程序
